1a4c7d8481625e5fc0f39687ad08c091fa7df047
Thor Amorim <thoramorim@gmail.com>
2026-03-03 23:33:39 -0300

    Update vite base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ferramenta-selecao-cursinho/",
+  base: "/ferramenta-de-selecao-cursinho-mtst/",
   plugins: [react()],
   build: {
     outDir: "build",
